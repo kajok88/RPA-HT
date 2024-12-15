92634ddca0be7e3f267574f236de32e0b2d1527b
kajok88 <skyborgg88@gmail.com>
2024-12-15 21:22:41 +0200

    Sending simple Telegram message

--- or-main/tasks.py
+++ or-main/tasks.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 @task
 def fetch_price():
+
     browser.configure(
         browser_engine="chromium",
         screenshot="only-on-failure",
@@ -13,7 +14,9 @@ def fetch_price():
     )
     # The URL of the product page you want to scrape
     # product_url = "https://www.verkkokauppa.com/fi/product/926983/Asus-AMD-Radeon-DUAL-RX7800XT-O16G-naytonohjain#list=OZCYkR8mw3qLYgY1LOT4k8bVCD8AFDJLYRst6TS0c8s1Oq8rShn8Aed58rfNJ8Ibcv8mwfb6Q9dELz7TKLl9X9LVYAb8IATz6QZCsLOTINLOTQF8rpXb8Q5ib8rfBa8As60YENo0Lk552LZvIHLHviK8QhHj86I8H8IAB58s18HLVY7K6fi6980BlF6tkLE8rSXa62IME6esyJ8WmXz8PtUWqJUY08rj0J8rah162sOvLOBik8IbNcr"
-    page = browser.goto("https://www.verkkokauppa.com/fi/product/926983/Asus-AMD-Radeon-DUAL-RX7800XT-O16G-naytonohjain#list=OZCYkR8mw3qLYgY1LOT4k8bVCD8AFDJLYRst6TS0c8s1Oq8rShn8Aed58rfNJ8Ibcv8mwfb6Q9dELz7TKLl9X9LVYAb8IATz6QZCsLOTINLOTQF8rpXb8Q5ib8rfBa8As60YENo0Lk552LZvIHLHviK8QhHj86I8H8IAB58s18HLVY7K6fi6980BlF6tkLE8rSXa62IME6esyJ8WmXz8PtUWqJUY08rj0J8rah162sOvLOBik8IbNcr")
+    product = "7800XT Näytönohjain"
+    link = "https://www.verkkokauppa.com/fi/product/926983/Asus-AMD-Radeon-DUAL-RX7800XT-O16G-naytonohjain#list=OZCYkR8mw3qLYgY1LOT4k8bVCD8AFDJLYRst6TS0c8s1Oq8rShn8Aed58rfNJ8Ibcv8mwfb6Q9dELz7TKLl9X9LVYAb8IATz6QZCsLOTINLOTQF8rpXb8Q5ib8rfBa8As60YENo0Lk552LZvIHLHviK8QhHj86I8H8IAB58s18HLVY7K6fi6980BlF6tkLE8rSXa62IME6esyJ8WmXz8PtUWqJUY08rj0J8rah162sOvLOBik8IbNcr"
+    page = browser.goto(link)
     try:
         # response = requests.get(product_url)
         # response.raise_for_status()  # Raise an error for bad HTTP responses
@@ -28,6 +31,16 @@ def fetch_price():
         #     print("Price element not found. Please check the selector.")
         hinta = page.locator(".jeFktI .sc-jnOGJG").text_content()
         print(hinta)
+        print(product + " " + hinta + " " + link)
+        send_message(product + " " + hinta + " " + link)
+        
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while fetching the product page: {e}")
+
+def send_message(mes):
+    TOKEN = ""
+    chat_id = ""
+    message = mes
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    print(requests.get(url).json()) # this sends the message
